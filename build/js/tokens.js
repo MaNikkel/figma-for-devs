89e5b4150c2f78add92c6bb9833fff9eec17a115
MaNikkel <MaNikkel@users.noreply.github.com>
2022-10-18 22:34:31 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,9 @@
 /**
  * Do not edit directly
- * Generated on Fri, 14 Oct 2022 11:37:10 GMT
+ * Generated on Tue, 18 Oct 2022 22:34:30 GMT
  */
 
 module.exports = {
-  "ColorWhite": "#ffffff",
-  "ColorGray200": "#c1c1c4",
-  "ColorGray500": "#5c5c5c",
-  "ColorGray800": "#1b1b1e",
-  "ColorGray900": "#121214",
-  "ColorPurple500": "#8257e6"
+  "ColorBlue400": "#8ec1ff",
+  "ColorBlue500": "#679ce7"
 };
